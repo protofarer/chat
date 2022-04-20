97b5fdd6aba7aa772e90cd411b88b9517cbb4bea
Kenny Baron <kennybaron@fastmail.com>
2022-04-20 18:44:01 -0400

    wip express

--- client.mjs
+++ client.mjs
@@ -1,4 +1,4 @@
-import WebSocket from 'ws';
+import WebSocket from 'isomorphic-ws';
 
 const url = `ws://localhost:8888`;
 
--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const session = require('express-session');
 const express=  require('express');
 const https = require('https');
 const uuid = require('uuid');
-require('dotenv')
+const fs = require('fs');
+require('dotenv').config();
 
 const { WebSocketServer } = require('ws');
 
 const app = express();
-const map = new Map();
+const sessionUsers = new Map();
 
 const PORT = process.env.PORT;
 
@@ -30,33 +31,54 @@ app.post('/login', (req, res) => {
 
 app.delete('/logout', (req, res) => {
   const ws = map.get(request.session.userId);
-  console.log('Destroying session');
+  console.log(`Destroying session for user id: ${request.session.userId }`);
   request.session.destroy(() => {
     if (ws) ws.close();
     response.send({ result: 'OK', message: 'Session destroyed' });
   });
 });
 
-const wss = new WebSocketServer({ PORT });
-console.log(`WSS listening on ${PORT}`)
+const server = https.createServer(
+  {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+  }, 
+  app
+);
+
+const wss = new WebSocketServer({ noServer: true, clientTracking: true });
+
 // wss.handleUpgrade(request, socket, head, function (ws) {
 //   wss.emit('connection', ws, request);
 // });
 
 wss.on('connection', function (ws, request) {
-  ws.on('message', function (message) {
-    console.log(`Received message: ${message}`);
-  })
+  // Upon connection
+  const userId = request.session.userId;
+  sessionUsers.set(userId, ws);
 
-  ws.on('close', function () {
-    console.log('Connection closed. Goodbye!');
-  })  
-
+  console.log(`user ${userId} connected, current connections: `);
+  console.table(wss.clients);
+  
   const message = {
     body: "Welcome to kenny.net chat",
     name: "kenny.net",
     time: Date.now(),
   }
   ws.send(JSON.stringify(message));
+
+  ws.on('message', function (message) {
+    console.log(`Received message: ${message} from ${userId}`);
+  })
+
+  ws.on('close', function () {
+    sessionUsers.delete(userId);
+    console.log('Client disconnected, current connections: '); 
+    console.table(wss.clients);
+  })  
+
 })
 
+server.listen(PORT, () => {
+  console.log(`listening on https://localhost:${PORT}`)
+});
