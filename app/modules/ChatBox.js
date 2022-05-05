59a5d8a108f712377f8efc7015d8310d0bd1eb01
Kenny Baron <kennybaron@fastmail.com>
2022-05-05 08:58:55 -0400

    wip

--- app/modules/ChatBox.js
+++ app/modules/ChatBox.js
@@ -1,55 +1,68 @@
 import { ui, state } from '../app.js'
 
-export function addChat({body, id, payload=null}) {
-  // TODO concatenates to existing state.chat
-  // chatBox.innerHTML updated to state.chat every time this
-  // fn invoked.
-  let newLine = `<li id="${id}" class="chatLine" `
-  newLine += `style="color:${payload?.sender === 'knet' ? 'green' : 'red'}">`
-  newLine += `${body}</li>`
+export function addChat({ id, time, sender, body, }) {
+  const chatBox = document.querySelector('#chatBox')
+  const line = DocumentFragment.createElement('li')
 
-  state.chatText += newLine
+  // id unique and calculated differently between server and client senders
+  line.id = id;
+
+  // Set css class according to sender
+  if (payload.sender === 'knet') {
+    line.className = "chatLineServer"
+  } else if (payload.sender === 'cli' || payload.sender === 'room-general') {
+    line.className = "chatLineClient"
+  } else {
+    line.className = "chatLine"
+  }
+
+  let lineText = ''
+  lineText += `(${time}) `;
+  lineText += ` <strong>[${sender}]</strong>:`
+
+  // TODO sanitize
+  // TODO replace html entities
+
   ui.chatBox.innerHTML += newLine
   ui.chatBox.scrollTop = chatBox.scrollHeight   // sets scrollTop to max value
 }
 
-export function addChatFromClient(body) {
-  let newBody = `(${new Date().toLocaleTimeString(
+export function addChatFromClient(lineTextBody) {
+  const id = `${Date.now()}-cli-${state.chatCounter}`
+
+  const time = new Date().toLocaleTimeString(
         'en-US', 
         { timeZoneName: 'short' }
-      )}) `
-  newBody += `<strong>[cli]:</strong> `
+      )
 
-  // TODO escape html entities ehre
-  newBody += `<pre class="chatLineBody">${body}</pre>`
+  const sender = 'cli'
   
   addChat({
-    body: newBody,
-    id: `${Date.now()}-cli-${state.chatCounter}`,
-    payload: {
-      sender: 'cli'
-    }
+    id,
+    time,
+    sender,
+    body: lineTextBody,
   })
+  // Client appends chatCounter to id, server handles its own
   state.chatCounter++;
 }
 
 export function addChatFromServer(action) {
-  let body = `(${new Date(action.payload.time).toLocaleTimeString(
-        'en-US', 
-        { timeZoneName: 'short' }
-      )}) `
-  body += `<strong>[${action.payload.sender}]:</strong> `
-
-  // TODO escape html entities ehre
-  body += `<pre class="chatLineBody">${action.payload.body}</pre>`
-
   let id = `${new Date(action.payload.time).getTime()}-`
   id += `${action.payload.sender}-`
   id += `${action.payload.chatCounter}`
 
+  let time = new Date(action.payload.time).toLocaleTimeString(
+        'en-US', 
+        { timeZoneName: 'short' }
+      )
+
+  const sender = action.payload.sender
+
   addChat({
-    body,
     id,
-    payload: action.payload,
+    time,
+    sender,
+    body: action.payload.body
   })
 }
